eedc21b95393069a75bf4ffc6185d4f5ae094307
Dhruva Hegde <dhruvahegde2021@gmail.com>
2026-02-13 17:52:22 +0530

    Revert " DAY-1 DSA"
    This reverts commit 340136e0942b9059bc550246204233fc66548d43.

---
+++ Foundation/2.Sum of two numbers.java
@@ -0,0 +1,9 @@
+// Pattern: Basics
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+// Notes: Simple addition
+class Solution {
+    public int sum(int num1, int num2) {
+        return (num1+num2);
+    }
+}
