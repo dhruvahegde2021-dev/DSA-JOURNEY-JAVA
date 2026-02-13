340136e0942b9059bc550246204233fc66548d43
Dhruva Hegde <dhruvahegde2021@gmail.com>
2026-02-13 17:37:02 +0530

    DAY-1 DSA
    Foundation section

--- Foundation/2.Sum of two numbers.java
+++
@@ -1,9 +0,0 @@
-// Pattern: Basics
-// Time Complexity: O(1)
-// Space Complexity: O(1)
-// Notes: Simple addition
-class Solution {
-    public int sum(int num1, int num2) {
-        return (num1+num2);
-    }
-}
