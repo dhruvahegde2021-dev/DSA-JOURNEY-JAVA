e88fe0e5f231714f351f7b270e04716020a3d97c
Dhruva Hegde <dhruvahegde2021@gmail.com>
2026-08-20 23:23:50 +0530

    add each num to minheap , if size of heap > k then remove element

---
+++ Heap and Priority Queue/1.Kth Largest Element in stream.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    PriorityQueue<Integer> minheap;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        this.minheap=new PriorityQueue<>();
+        for(int num:nums)
+        {
+            minheap.offer(num);
+            if(minheap.size()>k)
+                minheap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        minheap.offer(val);
+        if(minheap.size()>k)
+            minheap.poll();
+        return minheap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
