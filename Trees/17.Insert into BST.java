d68257175e7fccaefd10e1aa7e2ba8f9951ab724
Dhruva Hegde <dhruvahegde2021@gmail.com>
2026-08-26 19:13:14 +0530

    if val >= current , then add to right subT else add to left subT and return root

---
+++ Trees/17.Insert into BST.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * // Pattern: Trees
+ *  // Time Complexity: O(n)
+ *  // Space Complexity: O(1)
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null)
+            return new TreeNode(val);
+        
+        TreeNode current=root;
+        while(true)
+        {
+            if(val>=current.val)
+            {
+                if(current.right!=null)
+                    current=current.right;
+                else
+                    {
+                        current.right=new TreeNode(val);
+                        break;
+                    }
+            }
+
+            else
+            {
+                if(current.left!=null)
+                    current=current.left;
+                else
+                {
+                    current.left=new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
